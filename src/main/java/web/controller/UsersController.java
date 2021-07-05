83c083c6c34aafebf94480b3d067ef90fccf8d98
Evgeny <vasilkalin@gmail.com>
2021-07-05 18:55:12 +0200

    Work var.

--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -1,10 +1,8 @@
 package web.controller;
 
-//import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import web.dao.UserDao;
 import web.model.User;
