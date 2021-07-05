9f13869a8222783fa53973a5bc73e8bd2c018c2a
Evgeny <vasilkalin@gmail.com>
2021-07-05 18:40:40 +0200

    Work var. But don't work only delete user.

--- src/main/java/web/config/HiberConfig.java
+++ src/main/java/web/config/HiberConfig.java
@@ -10,8 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -45,17 +43,16 @@ public class HiberConfig {
         factoryBean.setPackagesToScan("web");
 
         Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+
+        //URGENT!
+        //don't work this string
+        //properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+
         properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
 
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-
-        factoryBean.setJpaVendorAdapter((JpaVendorAdapter) properties);
-
         factoryBean.setJpaVendorAdapter(vendorAdapter);
-        //factoryBean.setAnnotatedClasses(User.class);
-
         return factoryBean;
     }
 
