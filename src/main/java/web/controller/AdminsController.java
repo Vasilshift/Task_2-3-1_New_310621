9ce8664b0df4b30fb5ca2a37881145836a1750eb
Evgeny <vasilkalin@gmail.com>
2021-07-10 22:25:30 +0200

    After autorization shows page /hello

--- src/main/java/web/controller/AdminsController.java
+++ src/main/java/web/controller/AdminsController.java
@@ -1,20 +1,81 @@
-//package web.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//import web.model.Role;
-//import web.model.User;
-////import web.service.RoleService;
-//import web.service.UserService;
-//
-//import java.util.List;
-//
-//@Controller
-//@RequestMapping("/admin")
-//public class AdminsController {
-//
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.Role;
+import web.model.User;
+//import web.service.RoleService;
+import web.service.UserService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminsController {
+
+    private final UserService userService;
+
+    public AdminsController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public String userPage(Model model, int id) {
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String index(Model model) {
+        model.addAttribute("users", userService.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getUser(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "people/new";
+    }
+
+    @PostMapping("/")
+    public String add(@ModelAttribute("user") User user) {
+        userService.add(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "people/edit";
+    }
+
+    //	@RequestMapping(method = RequestMethod.PATCH)
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user) {
+        userService.update(user);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/";
+    }
+
+
+
+
+
+
+
 //    private UserService userService;
 ////    private RoleService roleService;
 ////
@@ -70,4 +131,4 @@
 //        return "redirect:/";
 //    }
 //
-//}
+}
--- src/main/java/web/controller/UsersController.java
+++ src/main/java/web/controller/UsersController.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/people")
 public class UsersController {
 
 	private final UserService userService;
@@ -21,67 +21,54 @@ public class UsersController {
 		this.userService = userService;
 	}
 
-	@GetMapping("/user")
-	public String userPage(Model model, int id) {
-		User user = userService.getUser(id);
-		model.addAttribute("user", user);
-		return "user";
-	}
-
-	@GetMapping("/")
-	public String index(Model model) {
-		model.addAttribute("users", userService.index());
-		return "people/index";
-	}
-
 	@GetMapping("/{id}")
 	public String show(@PathVariable("id") int id, Model model) {
 		model.addAttribute("user", userService.getUser(id));
 		return "people/show";
 	}
 
-	@GetMapping("/new")
-	public String newUser(@ModelAttribute("user") User user) {
-		return "people/new";
-	}
+//
+//	@GetMapping("/")
+//	public String index(Model model) {
+//		model.addAttribute("users", userService.index());
+//		return "people/index";
+//	}
+//
+//	@GetMapping("/{id}")
+//	public String show(@PathVariable("id") int id, Model model) {
+//		model.addAttribute("user", userService.getUser(id));
+//		return "people/show";
+//	}
+//
+//	@GetMapping("/new")
+//	public String newUser(@ModelAttribute("user") User user) {
+//		return "people/new";
+//	}
+//
+//	@PostMapping("/")
+//	public String add(@ModelAttribute("user") User user) {
+//		userService.add(user);
+//		return "redirect:/";
+//	}
+//
+//	@GetMapping("/{id}/edit")
+//	public String edit(Model model, @PathVariable("id") int id) {
+//		model.addAttribute("user", userService.getUser(id));
+//		return "people/edit";
+//	}
+//
+////	@RequestMapping(method = RequestMethod.PATCH)
+//	@PatchMapping("/{id}")
+//	public String update(@ModelAttribute("user") User user) {
+//		userService.update(user);
+//		return "redirect:/";
+//	}
+//
+//	@DeleteMapping("/{id}")
+//	public String delete(@PathVariable("id") int id) {
+//		userService.delete(id);
+//		return "redirect:/";
+//	}
 
-	@PostMapping("/")
-	public String add(@ModelAttribute("user") User user) {
-		userService.add(user);
-		return "redirect:/";
-	}
-
-	@GetMapping("/{id}/edit")
-	public String edit(Model model, @PathVariable("id") int id) {
-		model.addAttribute("user", userService.getUser(id));
-		return "people/edit";
-	}
-
-	@PatchMapping("/{id}")
-	public String update(@ModelAttribute("user") User user) {
-		userService.update(user);
-		return "redirect:/";
-	}
-
-	@DeleteMapping("/{id}")
-	public String delete(@PathVariable("id") int id) {
-		userService.delete(id);
-		return "redirect:/";
-	}
-
-	@RequestMapping(value = "login", method = RequestMethod.GET)
-	public String loginPage() {
-		return "login";
-	}
-
-	@RequestMapping(value = "hello", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC-SECURITY application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "hello";
-	}
 
 }
